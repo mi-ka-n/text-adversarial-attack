03e1067d8b24e777f52d04b47cbced0c6f8dc153
Zhaoqian Wang <zhw173@ucla.edu>
2022-06-08 23:50:34 -0700

    Fixed bug in evaluation script

--- run_attack.sh
+++ run_attack.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-for model in "dbpedia14"
+for model in "dbpedia14" "ag_news"
 do
     for idf in $(seq 10 10 20)
     do
-        python whitebox_attack.py --data_folder data --dataset ${model} --model gpt2 --finetune True --start_index 0 --num_samples 1 --gumbel_samples 100 --top_idf_percent ${idf} > "./attack_results/${model}_${idf}.txt" 2>&1
+        python whitebox_attack.py --data_folder data --dataset ${model} --model gpt2 --finetune True --start_index 0 --num_samples 100 --gumbel_samples 100 --top_idf_percent ${idf} > "./attack_results/${model}_${idf}.txt" 2>&1
     done
 done
 
-for model in "dbpedia14"
+for model in "dbpedia14" "ag_news"
 do
     for idf in $(seq 10 10 20)
     do
-        python evaluate_adv_samples.py --data_folder data --dataset ${model} --surrogate_model gpt2 --target_model gpt2 --finetune True --start_index 0 --num_samples 100 --gumbel_samples 1000 --top_idf_percent ${idf} > "./evaluate_results/${model}_${idf}.txt" 2>&1
+        python evaluate_adv_samples.py --data_folder data --dataset ${model} --surrogate_model gpt2 --target_model gpt2 --finetune True --start_index 0 --num_samples 100 --end_index 100 --gumbel_samples 1000 --top_idf_percent ${idf} > "./evaluate_results/${model}_${idf}.txt" 2>&1
     done
 done
